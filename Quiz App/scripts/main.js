121300df42aa065aba57e8c16b94fc4f599f243d
Phikiso, Obakeng <obakeng.phikiso@accenture.com>
2019-10-10 12:56:13 +0200

    update

--- Quiz App/scripts/main.js
+++ Quiz App/scripts/main.js
@@ -2,30 +2,40 @@ function submit() {
     var q1 = document.quiz.question1.value;
     var q2 = document.quiz.question2.value;
     var q3 = document.quiz.question3.value;
+    var q4 = document.quiz.question1.value;
+    var q5 = document.quiz.question2.value;
+    var q6 = document.quiz.question3.value;
+    var q7 = document.quiz.question1.value;
+    var q8 = document.quiz.question2.value;
+    var q9 = document.quiz.question3.value;
+    var q10 = document.quiz.question3.value;
+    var q11 = document.quiz.question3.value;
+    var q12 = document.quiz.question3.value;
+    var q13 = document.quiz.question3.value;
+    var q14 = document.quiz.question3.value;
+    var q15 = document.quiz.question3.value;
 
     var correct = 0;
+    var total = 15;
+    var answers = [3,2,1,1,2,1,2,3,1,3,2,3,3,3,2];
 
-    if(q1 == 2){
-        correct++;
-    }
-    if(q2 == 1){
-        correct++;
-    }
-    if(q3 == 3) {
-        correct++;
+    for(var i = 1; i <= total; i++) {
+        if(eval('q'+i) == answers[i - 1]) {
+            correct++;
+        }
     }
     
     var pictures = ["images/win.gif", "images/meh.jpeg", "images/lose.gif"];
 	var messages = ["Great job!", "That's just okay", "You need to do better next time"];
 	var score;
 
-    if(correct ==0) {
+    if(correct == 0) {
         score = 2;
     }
-    if(correct > 0 && correct < 3) {
+    if(correct > 0 && correct < 6) {
         score = 1;
     }
-    if (correct == 3) {
+    if (correct >= 6) {
         score = 0;
     }
     document.getElementById("after_submit").style.visibility = "visible";
